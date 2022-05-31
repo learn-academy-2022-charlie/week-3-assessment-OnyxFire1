4ae1cb89d0495c7450d5a76d1f808df4e9e8849d
OnyxFire1 <alexneil179@gmail.com>
2022-05-30 22:17:53 -0700

    week 3 assessment completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,8 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { sequenceExpression } = require("@babel/types")
+const { array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -15,42 +18,126 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
+// Pesudo Code: A number > 2 returns an array of numbers in the Fibonacci sequence.
+//The Fibonacci sequence adds the two previous numbers to get the next number in the sequence.
+// Run the test to get a good fail.
+// To get the good fail first invoke describe, it, and expect methods.
+// Describe will run the nested method, it will test the nested method, and expect will call the function.
+// const fibLength1 = 6
+// Expected output: [1, 1, 2, 3, 5, 8]
+// const fibLength2 = 10
+// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+//describe ("fibSeq", () => {
+//         const fibLength1 = 6
+// Expected output: [1, 1, 2, 3, 5, 8]
+//         const fibLength2 = 10
+// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    
+//it("takes in a number > 2 and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+//         expect(fibSeq(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//         expect(fibSeq(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+// })
+// })
+    
+//Good fail-ReferenceError: fibSeq is not defined
+//Peusodo Code
+
+// Will use array prototype .push to add one or more elements to   the end of the array.
+// The recursion sequece will alllow for the code to go back two numbers in the sequenceExpression.
+// const fibSeqLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
+// const fibSeqLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 
 // b) Create the function that makes the test pass.
 
 
+
+// const fibSeq = (length) => {
+//     let newArray = [1,1]
+//     for (let i=2; i<length; i++){
+//         newArray.push(newArray[i-2]+newArray[i-1])
+//     }
+//     return newArray
+
+// }
+// //PASS ./code-challenges.test.js
+
+
+
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
+// Peusdocode: Take in an array and return a new array with odd numbers from least to greatest.
+// Run the test to get a good fail.
+// To get the good fail first invoke describe, it, and expect methods.
+// Describe will run the nested method, it will test the nested method, and expect will call the function.
 
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+    describe ("fullA1", () => {
+    //const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+// Expected output: [-9, 7, 9, 199]
+    //const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
+
+    it("return a new array of odd numbers from least to greatest ", () => {
+    expect(fullA1(fullArray1)).toEqual([-9, 7, 9, 199])
+    expect(fullA2(fullArray2)).toEqual([-823, 7, 23,])
+    })
+    })
+
+//Good fail- ReferenceError: fullA1 is not defined
+
+const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+Expected output: [-9, 7, 9, 199]
+const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+Expected output: [-823, 7, 23]
 
 
 // b) Create the function that makes the test pass.
 
+const fullArr1 = () => {
+    return fullA1
+}
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+// Peusdocode: Return the array with the accumulatin sum, empty array should be empty.
+// Run the test to get a good fail.
+// To get the good fail first invoke describe, it, and expect methods.
+// Describe will run the nested method, it will test the nested method, and expect will call the function.
 // a) Create a test with expect statements for each of the variables provided.
 
+describe ("emptyArr", () => {
 const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-
+Excpected output: [2, 6, 51, 60]
+    
 const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
+Expected output: [0, 7, -1, 11]
 
 const numbersToAdd3 = []
+Expected output: []
+    it("return a new array of odd numbers from least to greatest ", () => {
+    expect(emptyArr(numbersToAdd1)).toEqual([2, 6, 51, 60])
+    expect(emptyArr2(numbersToAdd2)).toEqual([0, 7, -1, 11])
+    expect(emptyArr3(numbersToAdd3)).toEqual([])
+    })
+    })
+
+// const numbersToAdd1 = [2, 4, 45, 9]
+// // Excpected output: [2, 6, 51, 60]
+
+// const numbersToAdd2 = [0, 7, -8, 12]
+// // Expected output: [0, 7, -1, 11]
+
+// const numbersToAdd3 = []
 // Expected output: []
 
+Good fail -  ReferenceError: emptyArr is not defined
 
 // b) Create the function that makes the test pass.
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,61 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: Based on my understanding of 'this' keyword sits inside the code that defines the class.
 
-  Researched answer:
+  Researched answer: The this keyword in JavaScript alludes to an object, which object will depends on how the this keyword is called, 'this' can be referred to different objects based on how it is used. In a fuction it is a global object, in an event it refers to the element in receipt of it, and in an object method it refers to the object.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is how we interact with websites by displaying user interactions.
 
-  Researched answer:
+  Researched answer: React was created by Facebook, it is used to help developers build user interfaces as a structure of small pieces called components. A component is a combination of HTML and JavaScript that catches all of the logic that are in need of displaying a small segment of a larger user interface. Each of these components can be built up into back-to-back compounded parts of an app.
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: Render is the only lifecycle method required.
 
-  Researched answer:
+  Researched answer: The lifecycle in React can be monitored and manipulated. The render class method works in smart, class components only and displays the code that will go within the empty div after it is render from JSX.
 
 
 
 4. What is JSX? What is one syntax difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JavaScript XML is a combination of JavaScritpt and HTML.
 
-  Researched answer:
+  Researched answer: JSX allows us to write HTML in React, it makes it easier to write and add HTML in React. As an add-on to JS it comes up with a way to shape component rendering using known syntax.
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a JavaScript package manager that creates the file structure of a React app and manages the dependencies.
 
-  Researched answer:
+  Researched answer: Yarn is a new package manager that replaces the functionality for other package managers while remaining compatible. It has the same characteristics set as existing workflows while operating faster, more securely, and more reliably.
+
+The primary function of any package manager is to install some package — a piece of code that serves a particular purpose — from a global registry into a local environment. Each package may or may not depend on other packages. A typical project could have tens, hundreds, or even thousands of packages within its tree of dependencies.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: A nameless function.
 
-  Researched answer:
+  Researched answer: An anonymous function does not have any name connected with it, only the function keyword without the fucntion name is used.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Conditional rendering is a term to describe the ability to render different user interface (UI) markup if a condition is true or false. 
 
-2. Object-oriented programming:
+2. Object-oriented programming: A computer programming model that organizes software design around data, or objects, rather than functions and logic.
 
-3. Ruby:
+3. Ruby: A dynamic programming language that focuses on simplicity and productivity.
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby has anonymous functions similar to JavaScript. They are called blocks. Blocks can be created with the do/end keywords OR with {}. Blocks are code that is passed to a method.
 
-5. Ruby hashes:
+5. Ruby hashes: A container for data, the data is stored in key value pairs.
